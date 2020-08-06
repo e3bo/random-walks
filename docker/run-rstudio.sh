244e643bafe80ec6192200ca1d3a626a571dd831
Eamon O'Dea <odea35@gmail.com>
2020-08-06 17:10:17 -0400

    Update Docker image

--- docker/run-rstudio.sh
+++ docker/run-rstudio.sh
@@ -7,4 +7,4 @@ docker run \
 	-e USERID=1005 \
 	-e DISABLE_AUTH=true \
 	--mount type=bind,src=$HOME/src/random-walks,dst=/home/rstudio/work \
-	eamon/random-walks:2020-08-05 /init
+	eamon/random-walks:2020-08-06 /init
