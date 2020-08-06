8fe442e7cd624bb29ad9d71f4e0c6eedc7cb8b6b
Eamon O'Dea <odea35@gmail.com>
2020-08-06 17:12:05 -0400

    Add script to get versioned hopkins data

---
+++ pull_hopkins_ts_from_date.R
@@ -0,0 +1,30 @@
+
+library(magrittr)
+
+fdt <- 
+  ini::read.ini("forecast.ini")$forecast_pars$forecast_date %>%
+  str2lang() %>% 
+  eval()
+
+fdtm <- as.POSIXct(paste(fdt + 1, "0:00:00"), tz = "EST")
+
+xmlfile <- tempfile()
+url <- paste0("https://github.com/CSSEGISandData/COVID-19.git/trunk/",
+              "csse_covid_19_data/csse_covid_19_time_series")
+cmd <- paste("svn log ", url, " --xml >", xmlfile)
+system(cmd)
+log <- xml2::read_xml(xmlfile)
+datetimes <- 
+  xml2::xml_find_all(log, "//logentry/date") %>% 
+  xml2::xml_text() %>%
+  lubridate::as_datetime()
+is_later <- datetimes > fdtm
+contemp <- match(FALSE, is_later)
+xpath <- paste0("//logentry[", contemp, "]")
+revision <- 
+  xml2::xml_find_all(log, xpath) %>% 
+  xml2::xml_attr("revision")
+url2 <- paste0(url, "@r", revision)
+system("mkdir hopkins")
+cmd2 <- paste0("svn export ", url2, " ./hopkins/", fdt)
+system(cmd2)
