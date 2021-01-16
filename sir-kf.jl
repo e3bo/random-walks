df79708458a7af270f957ae8467edd2dfa7d9897
Eamon O'Dea <odea35@gmail.com>
2021-01-15 19:33:52 -0500

    Simple version of kalman filter implemented

--- sir-kf.jl
+++ sir-kf.jl
@@ -8,43 +8,49 @@ ytk = similar(times)
 s = similar(times)
 n = length(z)
 
+using LinearAlgebra
 
-## hacked example from Kalman.jl, by Moritz Schauer (GitHub user mschauer)
-using Kalman, GaussianDistributions, LinearAlgebra
-using GaussianDistributions: ⊕ # independent sum of Gaussian r.v.
-using Statistics
+function obj(a)
+    # prior for time 0
+    x0 = [-1., 1.]
+    p0 = Matrix(1.0I, 2, 2)
 
-# prior for time 0
-x0 = [-1., 1.]
-P0 = Matrix(1.0I, 2, 2)
+    # dynamics
+    Φ = [0.8 0.2; -0.1 a]
+    q = [0.2 0.0; 0.0 0.5]
 
-# dynamics
-Φ = [0.8 0.2; -0.1 0.8]
-b = zeros(2)
-Q = [0.2 0.0; 0.0 0.5]
+    # observation
+    h = [1.0 0.0]
+    r = Matrix(0.3I, 1, 1)
 
-# observation
-H = [1.0 0.0]
-R = Matrix(0.3I, 1, 1)
+    # (mock) data
+    z = [[-1.77], [-0.78], [-1.28], [-1.06], [-3.65], [-2.47], [-0.06], [-0.91], [-0.80], [1.48]]
 
-# (mock) data
-ys = [[-1.77], [-0.78], [-1.28], [-1.06], [-3.65], [-2.47], [-0.06], [-0.91], [-0.80], [1.48]]
+    # filter (assuming first observation at time 1)
+    n = length(z)
 
+    s = zeros(Float64, 1, 1, n)
+    ytkkmo = zeros(Float64, 1, n)
+    k = zeros(Float64, 2, n)
+    xkk = zeros(Float64, 2, n)
+    xkkmo = zeros(Float64, 2, n)
+    pkk = zeros(Float64, 2, 2, n)
+    pkkmo = zeros(Float64, 2, 2, n)
 
-# filter (assuming first observation at time 1)
-N = length(ys)
-
-s = zeros(Float64, 1, 1, N)
-yres = zeros(Float64, 1, N)
-p = Gaussian(x0, P0)
-ps = [p] # vector of filtered Gaussians
-for i in 1:N
-    global p
-    # predict
-    p = Φ*p ⊕ Gaussian(zero(x0), Q) #same as Gaussian(Φ*p.μ, Φ*p.Σ*Φ' + Q)
-    # correct
-    p, yres[:,i], s[:,:,i] = Kalman.correct(Kalman.JosephForm(), p, (Gaussian(ys[i], R), H))
-    push!(ps, p) # save filtered density
+    for i in 1:n
+        if (i == 1)
+            xkkmo[:,i] = Φ*x0
+            pkkmo[:,:,i] = Φ*p0*Φ' + q
+        else
+            xkkmo[:,i] = Φ*xkk[:,i - 1]
+            pkkmo[:,:,i] = Φ*pkk[:,:,i-1]*Φ' + q
+        end
+        s[:,:,i] = h * pkkmo[:,:,i] * h' + r
+        k[:,i] = pkkmo[:,:,i] * h' / s[:,:,i]
+        ytkkmo[:,i] = z[i] + h * reshape(xkkmo[:,i], 2, 1)
+        xkk[:,i] = reshape(xkkmo[:,i], 2, 1) + reshape(k[:,i], 2, 1) * ytkkmo[:,i]
+        pkk[:,:,i] = (I - reshape(k[:,i], 2, 1) * h) * pkkmo[:,:,i]
+    end
+    nll = 0.5 * (sum(ytkkmo[1,:] .^2 ./ s[1,1,:] + map(log, s[1,1,:])) + n * log(2 * pi))
+    (nll, ytkkmo, xkkmo)
 end
-
-nll = 0.5 * (sum(yres[1,:] .^2 ./ s[1,1,:] + map(log, s[1,1,:])) + N * log(2 * pi))
