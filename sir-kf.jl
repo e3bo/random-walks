e720d68cd3ab4cfc52abff5a33a316f273ad2dc5
Eamon O'Dea <odea35@gmail.com>
2021-01-18 00:15:32 -0500

    Obtaining reasonable SIR estimates

--- sir-kf.jl
+++ sir-kf.jl
@@ -1,9 +1,9 @@
 using LinearAlgebra
 using Optim
 
-function obj(pvar::Vector; γ::Float64 = 365.25 / 9, dt::Float64 = 1 / 365.25, ι::Float64 = 0.1, η::Float64 = 365.25 / 4, N::Float64 = 20e6, ρ::Float64 = 0.4, τ::Float64 = 0.01)
+function obj(pvar::Vector; γ::Float64 = 365.25 / 9, dt::Float64 = 1 / 365.25, ι::Float64 = 0., η::Float64 = 365.25 / 4, N::Float64 = 20e6, ρ::Float64 = 0.4, τ::Float64 = 0.01)
     # prior for time 0
-    x0 = [19e6; 1e4; 1e4; 0]
+    x0 = [19e6; pvar[2]; pvar[3]; 0]
     p0 = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 0]
 
     dstate = size(x0, 1)
@@ -14,7 +14,7 @@ function obj(pvar::Vector; γ::Float64 = 365.25 / 9, dt::Float64 = 1 / 365.25, �
     r = Matrix(undef, dobs, dobs)
 
     # data
-    z = [[1393], [1360], [1836], [1592], [1447], [1143]]
+    z = [[754], [665], [955], [908], [1005], [866], [834], [1189], [1000], [1382], [1598], [1731], [1222], [933], [1393], [1360], [1836], [1592], [1447], [1143]]
 
     # filter (assuming first observation at time 1)
     nobs = length(z)
@@ -70,15 +70,16 @@ function obj(pvar::Vector; γ::Float64 = 365.25 / 9, dt::Float64 = 1 / 365.25, �
         r[1,1] = z[i][1] * τ 
         Σ[:,:,i] = h * pkkmo[:,:,i] * h' + r
         k[:,i] = pkkmo[:,:,i] * h' / Σ[:,:,i]
-        ytkkmo[:,i] = z[i] + h * reshape(xkkmo[:,i], dstate, 1)
+        ytkkmo[:,i] = z[i] - h * reshape(xkkmo[:,i], dstate, 1)
         xkk[:,i] = reshape(xkkmo[:,i], dstate, 1) + reshape(k[:,i], dstate, 1) * ytkkmo[:,i]
         pkk[:,:,i] = (I - reshape(k[:,i], dstate, 1) * h) * pkkmo[:,:,i]
     end        
     nll = 0.5 * (sum(ytkkmo[1,:] .^2 ./ Σ[1,1,:] + map(log, Σ[1,1,:])) + nobs * log(2 * pi))
+    #nll, ytkkmo, Σ, xkkmo
     nll
 end
 
-ans0 = optimize(obj, [0], [100], [0.3], Fminbox(BFGS())) # works, but no autodiff
-ans1 = optimize(obj, [100.0], LBFGS()) # works if init is close to minimizer
-ans2 = optimize(obj, [1.0], LBFGS(); autodiff = :forward) # reduced iterations
-ans3 = optimize(obj, [0], [500], [8.], Fminbox(LBFGS()); autodiff = :forward) # can start far away 
+ans0 = optimize(obj, [0, 100, 100], [500, 1e5, 1e5], [8., 1e3, 1e3], Fminbox(BFGS())) # works, but no autodiff
+#ans1 = optimize(obj, [100.0], LBFGS()) # works if init is close to minimizer
+#ans2 = optimize(obj, [1.0], LBFGS(); autodiff = :forward) # reduced iterations
+ans3 = optimize(obj, [0, 100, 100], [500, 1e5, 1e5], [8., 1e3, 1e3], Fminbox(LBFGS()); autodiff = :forward) # can start far away 
