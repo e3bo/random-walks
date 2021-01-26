c1e23aa87807e32947088eb7f8d682b7ec751295
Eamon O'Dea <odea35@gmail.com>
2021-01-26 14:57:26 -0500

    Add trajectory plotting stages

--- evaluate.sh
+++ evaluate.sh
@@ -3,13 +3,17 @@
 set -e
 
 dvc run \
-    -d CEID-InfectionKalman \
-    -d CEID-Walk \
-    -d COVIDhub-Ensemble \
-    -d evaluate-forecasts.R \
-    -o figure \
-    -o evaluate-forecasts.md \
-    -o evaluate-forecasts.html \
+    -d pull-other-forecasts.R \
+    -o other-model-forecasts.rds \
     --force \
-    -n evaluate \
-    'Rscript -e "knitr::spin(\"evaluate-forecasts.R\")"'
+    -n pull-archived-forecasts \
+    ./pull-other-forecasts.R
+    
+dvc run \
+    -d other-model-forecasts.rds \
+    -d CEID-InfectionKalman \
+    -d make-trajectory-plots.R \
+    -o trajectories.png \
+    --force \
+    -n make-trajectory-plots \
+    ./make-trajectory-plots.R
