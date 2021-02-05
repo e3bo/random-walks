a8124f777c749ef37332c6e1ea0deb9ad009bf40
Eamon O'Dea <odea35@gmail.com>
2021-02-05 12:39:22 -0500

    Make interface of nll compatible with gpnet

--- InfectionKalmanRegularization.R
+++ InfectionKalmanRegularization.R
@@ -35,8 +35,8 @@ tau_init <- case_data$reports %>% tail(n = wsize) %>% var()
 
 pvar_df <- tribble(
   ~par, ~init, ~lower, ~upper,
-  "I_0", log(1e4), log(10), log(1e5),
-  "tau", log(tau_init), log(tau_init * 1e-8), log(tau_init * 10)
+  "logI_0", log(1e4), log(10), log(1e5),
+  "logtau", log(tau_init), log(tau_init * 1e-8), log(tau_init * 10)
 ) %>% 
   bind_rows(tibble(par = "b1",
                    init = log(gamma), 
@@ -47,14 +47,10 @@ pvar_df <- tribble(
                    lower = -10,
                    upper = 10))
 
-the_t0 <- rev(case_data$time)[wsize + 1]
-
-iterate_f_and_P <- function(xhat, PN, pvec, beta_t, time.steps){
-  P <- PN / pvec["N"]
+iterate_f_and_P <- function(xhat, PN, eta, gamma, N, beta_t, time.steps){
+  P <- PN / N
   dt <- diff(time.steps)
-  vf <-  with(as.list(c(pvec, xhat, beta_t)), {
-    eta <- pvec["eta"]
-    gamma <- pvec["gamma"]
+  vf <-  with(as.list(c(xhat, beta_t)), {
     F <- rbind(
       c(-beta_t * I / N,    0,-beta_t * S / N, 0),
       c(beta_t * I / N,-eta,  beta_t * S / N, 0),
@@ -71,7 +67,7 @@ iterate_f_and_P <- function(xhat, PN, pvec, beta_t, time.steps){
     
     dS <- (-beta_t * S * I / N)
     dE <- (beta_t * S * I / N - eta * E)
-    dI <- (iota + eta * E -  gamma * I)
+    dI <- (eta * E -  gamma * I)
     dC <- (gamma * I)
     dP <-  F %*% P + P %*% t(F) + Q
     
@@ -82,50 +78,59 @@ iterate_f_and_P <- function(xhat, PN, pvec, beta_t, time.steps){
   xhat_new[xhat_new < 0] <- 0
   P_new <- P + vf$dP * dt
   names(xhat_new) <- c("S", "E", "I", "C")
-  PN_new <- P_new * pvec["N"]
+  PN_new <- P_new * N
   list(xhat = xhat_new, PN = PN_new)
 }
 
+param_map <- function(x, w){
+  ret <- list()
+  is_spline_par <- grepl("^b[0-9]+$", names(w))
+  tmp <- w[is_spline_par]
+  bpars_diff <- tmp[order(as.integer(str_remove(names(tmp), "^b")))]
+  bpars_diff[1] <- exp(bpars_diff[1]) ## transform
+  stopifnot(length(bpars_diff) == nrow(x))
+  ret$bpars <- cumsum(bpars_diff)
+  
+  I_0 <- exp(w["logI_0"])
+  E_0 <- I_0 * w["gamma"] / w["eta"]
+  S_0 <- w["N"] - E_0 - I_0
+  
+  ret$xhat0 = c(S_0, E_0, I_0, 0)
+  names(ret$xhat0) <- c("S", "E", "I", "C")
+  
+  ret$rho1 <- w["rho1"]
+  ret$tau <- exp(w["logtau"])
+  ret$times <- x[, 1]
+  ret$eta <- w["eta"]
+  ret$gamma <- w["gamma"]
+  ret$N <- w["N"]
+  ret$t0 <- w["t0"]
+  ret
+}
+
 kfnll <-
-  function(pvar,
-           pfixed,
-           cdata,
+  function(bpars,
+           xhat0,
+           rho1, 
+           tau,
+           eta,
+           gamma,
+           N,
+           z,
            t0,
+           times, 
            Phat0 = diag(c(1, 1, 1, 0)),
-           just_nll = TRUE,
-           nsim = 10,
-           fets = NULL,
-           Rzzero = 1e6,
-           fet_zero_cases = "daily") {
-    p <- c(exp(pvar), pfixed)
+           just_nll = TRUE) {
     
-    is_spline_par <- grepl("^b[0-9]+$", names(p))
-    tmp <- p[is_spline_par]
-    bpars_diff <- tmp[order(as.integer(str_remove(names(tmp), "^b")))]
-    bpars_diff[-1] <- log(bpars_diff[-1]) ## backtransform step sizes so that they can be negative
-    stopifnot(length(bpars_diff) == nrow(cdata))
-    bpars <- cumsum(bpars_diff)
-    
-    is_wday_par <- grepl("rho[1-7]", names(p))
-    tmp <- p[is_wday_par]
-    wpars <- tmp[order(names(tmp))]
-    stopifnot(length(wpars) == 1)
-    
-    xhat0 = c(p[c("S_0", "E_0", "I_0")], 0)
-    names(xhat0) <- c("S", "E", "I", "C")
-    
-    T <- nrow(cdata)
+    T <- length(z)
     stopifnot(T > 0)
-    z <- cdata$smooth
-    times <- cdata$time
-    wday <- cdata$wday
     
     ytilde_kk <- ytilde_k <- S <- array(NA_real_, dim = c(1, T))
     K <- xhat_kk <- xhat_kkmo <- array(NA_real_, dim = c(4, T))
     rownames(xhat_kk) <- rownames(xhat_kkmo) <- names(xhat0)
     P_kk <- P_kkmo <- array(NA_real_, dim = c(4, 4, T))
     
-    H <-matrix(c(0, 0, 0, p["rho1"]), ncol = 4)
+    H <-matrix(c(0, 0, 0, rho1), ncol = 4)
     for (i in seq(1, T)) {
       if (i == 1) {
         xhat_init <- xhat0
@@ -136,18 +141,17 @@ kfnll <-
         PNinit <- P_kk[, , i - 1]
         time.steps <- c(times[i - 1], times[i])
       }
-      R <- p["tau"]
-      if (z[i] < 1){
-        R <- Rzzero * p["tau"]
-      }
-      if (TRUE){
-        xhat_init["C"] <- 0
-        PNinit[, 4] <- PNinit[4, ] <- 0
-      }
+      R <- tau
+      
+      xhat_init["C"] <- 0
+      PNinit[, 4] <- PNinit[4, ] <- 0
+      
       XP <- iterate_f_and_P(
         xhat_init,
         PN = PNinit,
-        pvec = p,
+        eta = eta,
+        gamma = gamma,
+        N = N,
         beta_t = bpars[i],
         time.steps = time.steps
       )
@@ -169,62 +173,6 @@ kfnll <-
     nll <-
       0.5 * sum(ytilde_k ^ 2 / S + log(S) + log(2 * pi)) 
     if (!just_nll) {
-      if (!is.null(fets)) {
-        sim_means <-
-          sim_cov <- matrix(NA, nrow = (nrow(fets)), ncol = nsim)
-        for (j in seq_len(nsim)) {
-          bpars_fet <- numeric(nrow(fets))
-          bpars_fet[1] <- p["gamma"] + (bpars[T] - p["gamma"] )* p["a"] + rnorm(n = 1, sd = p["betasd"])
-          rand <- runif(1)
-          if (rand > 0.66){
-            bpars_fet[1] <- bpars_fet[1] * 1.5
-          } else if (rand < 0.33){
-            bpars_fet[1] <- bpars_fet[1] * 0.5
-          }
-          if (length(bpars_fet) > 1){
-            for (jj in seq(2, length(bpars_fet))){
-              bpars_fet[jj] <- p["gamma"] + (bpars_fet[jj - 1]  - p["gamma"]) * p["a"] + rnorm(n = 1, sd = p["betasd"])
-            }
-          }
-          bpars_fet[bpars_fet < 0] <- 0
-          xhat_init <- xhat_kk[, T]
-          PNinit <- P_kk[, , T]
-          if (fet_zero_cases == "daily" || fets$target_wday[1] == 1){
-            xhat_init["C"] <- 0
-            PNinit[, 4] <- PNinit[4, ] <- 0
-          }
-          XP <- iterate_f_and_P(
-            xhat_init,
-            PN = PNinit,
-            pvec = p,
-            beta_t = bpars_fet[1],
-            time.steps = c(times[T], fets$target_end_times[1])
-          )
-          sim_means[1, j] <- H %*% XP$xhat
-          sim_cov[1, j] <- H %*% XP$PN %*% t(H) + p["tau"]
-          for (i in seq_along(fets$target_end_times[-1])) {
-            xhat_init <- XP$xhat
-            PNinit <- XP$PN
-            if (fet_zero_cases == "daily" || fets$target_wday[i + 1] == 1){
-              xhat_init["C"] <- 0
-              PNinit[, 4] <- PNinit[4, ] <- 0
-            }
-            XP <-
-              iterate_f_and_P(
-                xhat_init,
-                PN = PNinit,
-                pvec = p,
-                beta_t = bpars_fet[i + 1],
-                time.steps = c(fets$target_end_times[i], fets$target_end_times[i + 1])
-              )
-            sim_means[i + 1, j] <- H %*% XP$xhat
-            sim_cov[i + 1, j] <-
-              H %*% XP$PN %*% t(H) + p["tau"]
-          }
-        }
-      } else {
-        sim_means <- sim_cov <- NULL
-      }
       list(
         nll = nll,
         xhat_kkmo = xhat_kkmo,
@@ -241,21 +189,45 @@ kfnll <-
     }
   }
 
-pvar <- pvar_df$init
-names(pvar) <- pvar_df$par
+winit0 <- pvar_df$init
+names(winit0) <- pvar_df$par
 
-pfixed <- c(
+winit <- c(winit0,
   N = 20e6,
-  betasd = 2.,
-  a = .97,
   rho1 = 0.4,
   gamma = 365.25 / 9, 
   eta = 365.25 / 4,
-  iota = 0
+  t0 = rev(case_data$time)[wsize + 1]
 )
 
-pfixed["E_0"] <- pvar["I_0"] * pfixed["gamma"] / pfixed["eta"]
-pfixed["S_0"] <- pfixed["N"] - pfixed["E_0"] - pvar["I_0"]
+wind <- tail(case_data, n = wsize)
+y <- wind$smooth
+x <- matrix(wind$time, ncol = 1)
+
+calc_kf_nll <- function(w, x, y, pm) {
+  p <- pm(x, w)
+  nll <- kfnll(
+    bpars = p$bpars,
+    xhat0 = p$xhat0,
+    rho1 = p$rho1,
+    tau = p$tau,
+    eta = p$eta,
+    gamma = p$gamma,
+    N = p$N,
+    z = y,
+    t0 = p$t0,
+    times = p$times,
+    just_nll = TRUE
+  )
+  nll
+}
+
+
+calc_kf_nll(winit, x, y, param_map)
+
+get_gpnet()
+
+
 
 if(FALSE){
   tictoc::tic("optimization")
