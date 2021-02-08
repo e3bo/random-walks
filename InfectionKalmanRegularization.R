3a8686d559c15070414ab2225a16ec92c462ad92
Eamon O'Dea <odea35@gmail.com>
2021-02-08 12:54:03 -0500

    Sort functions from main procedure of script

--- InfectionKalmanRegularization.R
+++ InfectionKalmanRegularization.R
@@ -6,51 +6,77 @@ suppressPackageStartupMessages(library(tidyverse))
 source("covidhub-common.R")
 source("regularization.R")
 
+## main functions
 
-forecast_date <- Sys.getenv("fdt", unset = "2020-10-12")
-forecast_loc <- Sys.getenv("loc", unset = "36")
-hopdir <- file.path("hopkins", forecast_date)
-tictoc::tic("data loading")
-tdat <- load_hopkins(hopdir, weekly = FALSE) 
-tictoc::toc()
-
-ltdat <- tdat %>% filter(location == forecast_loc) %>% 
-  filter(target_type == "day ahead inc case")
-
-ltdat2 <- ltdat %>% mutate(time = lubridate::decimal_date(target_end_date))
-case_data <- ltdat2 %>% ungroup() %>% 
-  mutate(wday = lubridate::wday(target_end_date)) %>%
-  select(target_end_date, time, wday, value) %>% 
-  rename(reports = value)
-
-target_end_dates <- max(case_data$target_end_date) + lubridate::ddays(1) * seq(1, 28)
-target_end_times <- lubridate::decimal_date(target_end_dates)
-target_wday <- lubridate::wday(target_end_dates)
-
-
-moving_average <- function(x, n = 7) {
-  stats::filter(x, rep(1 / n, n), sides = 1)
+create_forecast_df <- function(means,
+                               vars,
+                               target_type = "cases",
+                               fdt = forecast_date,
+                               location) {
+  # takes in a vector of means and variance for the normal predicted
+  # distribution of the next n weeks, and outputs a representation of
+  # this forecast in the standard covidhub format
+  if (target_type == "cases") {
+    probs <- c(0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975)
+    maxn <- 8
+    targ_string <- " wk ahead inc case"
+  } else{
+    stop("not implemented")
+    probs <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
+    if (target_type == "hosps") {
+      maxn <- 131
+    } else{
+      maxn <- 20
+    }
+  }
+  n <- nrow(means)
+  stopifnot(nrow(vars) == n)
+  stopifnot(n <= maxn)
+  wk_ahead <- seq_len(n)
+  targets <- paste0(wk_ahead, targ_string)
+  dte <- lubridate::ymd(fdt)
+  fdt_wd <- lubridate::wday(dte) # 1 = Sunday, 7 = Sat.
+  fdt_sun <- lubridate::ymd(dte) - (fdt_wd - 1)
+  take_back_step <- fdt_wd <= 2
+  if (take_back_step) {
+    week0_sun <- fdt_sun - 7
+  } else {
+    week0_sun <- fdt_sun
+  }
+  t1 <- expand_grid(quantile = probs, h = wk_ahead) %>%
+    mutate(target = paste0(h, targ_string),
+           target_end_date = week0_sun + 6 + h * 7) %>%
+    add_column(
+      location = as.character(location),
+      forecast_date = fdt,
+      type = "quantile"
+    ) %>%
+    mutate(q1 = map2_dbl(
+      quantile,
+      h,
+      ~ KScorrect::qmixnorm(
+        p = .x,
+        mean = means[.y,],
+        sd = sqrt(vars[.y,]),
+        pro = rep(1, ncol(means)),
+        expand = 0.0
+      )
+    ),
+    value = ifelse(q1 < 0, 0, q1)) %>%
+    mutate(value = format(value, digits = 4),
+           quantile = as.character(quantile))
+  t2 <- t1 %>% filter(quantile == "0.5") %>%
+    mutate(type = "point",
+           quantile = NA_character_)
+  bind_rows(t1, t2) %>% select(forecast_date,
+                               target,
+                               target_end_date,
+                               location,
+                               type,
+                               quantile,
+                               value)
 }
 
-case_data$smooth <- moving_average(case_data$reports)
-
-wsize <- 60
-gamma <- 365.25/9
-tau_init <- case_data$reports %>% tail(n = wsize) %>% var()
-
-pvar_df <- tribble(
-  ~par, ~init, ~lower, ~upper,
-  "logI_0", log(1e4), log(10), log(1e5),
-  "logtau", log(tau_init), log(tau_init * 1e-8), log(tau_init * 10)
-) %>% 
-  bind_rows(tibble(par = "b1",
-                   init = log(gamma), 
-                   lower = log(0.1 * gamma),
-                   upper = log(4 * gamma))) %>%
-  bind_rows(tibble(par = paste0("b", 1 + seq_len(wsize - 1)),
-                   init = 0,
-                   lower = -10,
-                   upper = 10))
 
 iterate_f_and_P <- function(xhat, PN, eta, gamma, N, beta_t, time.steps){
   P <- PN / N
@@ -152,7 +178,7 @@ kfnll <-
         (diag(4) - K[, i, drop = FALSE] %*% H) %*% P_kkmo[, , i]
       ytilde_kk[i] <- z[i] - H %*% xhat_kk[, i, drop = FALSE]
     }
- 
+    
     nll <-
       0.5 * sum(ytilde_k ^ 2 / S + log(S) + log(2 * pi)) 
     if (!just_nll) {
@@ -226,6 +252,92 @@ kfnll <-
     }
   }
 
+moving_average <- function(x, n = 7) {
+  stats::filter(x, rep(1 / n, n), sides = 1)
+}
+
+calc_kf_nll <- function(w, x, y, pm) {
+  p <- pm(x, w)
+  nll <- kfnll(
+    bpars = p$bpars,
+    xhat0 = p$xhat0,
+    rho1 = p$rho1,
+    tau = p$tau,
+    eta = p$eta,
+    gamma = p$gamma,
+    N = p$N,
+    z = y,
+    t0 = p$t0,
+    times = p$times,
+    just_nll = TRUE
+  )
+  nll
+}
+
+kf_nll_details <- function(w, x, y, pm, lambda) {
+  p <- pm(x, w)
+  nll <- kfnll(
+    bpars = p$bpars,
+    xhat0 = p$xhat0,
+    rho1 = p$rho1,
+    tau = p$tau,
+    eta = p$eta,
+    gamma = p$gamma,
+    N = p$N,
+    z = y,
+    t0 = p$t0,
+    times = p$times,
+    fet = fet,
+    lambda = lambda,
+    just_nll = FALSE,
+    fet_zero_cases = "weekly",
+    nsim = 10
+  )
+  nll
+}
+
+## main script
+
+forecast_date <- Sys.getenv("fdt", unset = "2020-10-12")
+forecast_loc <- Sys.getenv("loc", unset = "36")
+hopdir <- file.path("hopkins", forecast_date)
+tictoc::tic("data loading")
+tdat <- load_hopkins(hopdir, weekly = FALSE) 
+tictoc::toc()
+
+ltdat <- tdat %>% filter(location == forecast_loc) %>% 
+  filter(target_type == "day ahead inc case")
+
+ltdat2 <- ltdat %>% mutate(time = lubridate::decimal_date(target_end_date))
+case_data <- ltdat2 %>% ungroup() %>% 
+  mutate(wday = lubridate::wday(target_end_date)) %>%
+  select(target_end_date, time, wday, value) %>% 
+  rename(reports = value)
+
+target_end_dates <- max(case_data$target_end_date) + lubridate::ddays(1) * seq(1, 28)
+target_end_times <- lubridate::decimal_date(target_end_dates)
+target_wday <- lubridate::wday(target_end_dates)
+
+case_data$smooth <- moving_average(case_data$reports)
+
+wsize <- 60
+gamma <- 365.25/9
+tau_init <- case_data$reports %>% tail(n = wsize) %>% var()
+
+pvar_df <- tribble(
+  ~par, ~init, ~lower, ~upper,
+  "logI_0", log(1e4), log(10), log(1e5),
+  "logtau", log(tau_init), log(tau_init * 1e-8), log(tau_init * 10)
+) %>% 
+  bind_rows(tibble(par = "b1",
+                   init = log(gamma), 
+                   lower = log(0.1 * gamma),
+                   upper = log(4 * gamma))) %>%
+  bind_rows(tibble(par = paste0("b", 1 + seq_len(wsize - 1)),
+                   init = 0,
+                   lower = -10,
+                   upper = 10))
+
 winit <- pvar_df$init
 names(winit) <- pvar_df$par
 
@@ -266,25 +378,7 @@ wind <- tail(case_data, n = wsize)
 y <- wind$smooth
 x <- matrix(wind$time, ncol = 1)
 
-calc_kf_nll <- function(w, x, y, pm) {
-  p <- pm(x, w)
-  nll <- kfnll(
-    bpars = p$bpars,
-    xhat0 = p$xhat0,
-    rho1 = p$rho1,
-    tau = p$tau,
-    eta = p$eta,
-    gamma = p$gamma,
-    N = p$N,
-    z = y,
-    t0 = p$t0,
-    times = p$times,
-    just_nll = TRUE
-  )
-  nll
-}
-
-calc_kf_nll(winit, x, y, param_map)
+#calc_kf_nll(winit, x, y, param_map)
 pen_factor <- rep(1, length(winit))
 pen_factor[1:3] <- 0
 
@@ -295,7 +389,7 @@ rpath <-
     y = y,
     calc_convex_nll = calc_kf_nll,
     param_map = param_map,
-    nlambda = 2,
+    nlambda = 10,
     lambda.min.ratio = 0.1,
     penalty.factor = pen_factor,
     winit = winit,
@@ -305,34 +399,13 @@ tictoc::toc()
 
 # View diagnostics
 
-penind <- 2
+penind <- 10
 wfit <- c(rpath$a0[,penind], rpath$beta[,penind])
 names(wfit)[4:length(wfit)] <- paste0("b", 2:wsize)
 
 
 fet <- tibble(target_end_times, target_wday, target_end_dates)
 
-kf_nll_details <- function(w, x, y, pm, lambda) {
-  p <- pm(x, w)
-  nll <- kfnll(
-    bpars = p$bpars,
-    xhat0 = p$xhat0,
-    rho1 = p$rho1,
-    tau = p$tau,
-    eta = p$eta,
-    gamma = p$gamma,
-    N = p$N,
-    z = y,
-    t0 = p$t0,
-    times = p$times,
-    fet = fet,
-    lambda = lambda,
-    just_nll = FALSE,
-    fet_zero_cases = "weekly",
-    nsim = 10
-  )
-  nll
-}
 
 dets <- kf_nll_details(wfit, x, y, param_map, rpath$lambda[penind])
 par(mfrow = c(1,1))
@@ -343,80 +416,13 @@ par(mfrow = c(4, 1))
 tgrid <- tail(case_data$time, n = wsize)
 
 plot(tgrid, tail(case_data$smooth, n = wsize), xlab = "Time", ylab = "Cases")
-lines(tgrid, dets$xhat_kkmo["C",] * fixed["rho1"])
+lines(tgrid, dets$xhat_kkmo["C",] * wfixed["rho1"])
 
 plot(tgrid, dets$S, log = "y", xlab = "Time", ylab = "Variance in smoother")
 plot(tgrid, dets$ytilde_k, xlab = "Time", ylab = "Residual in process 1-ahead prediction")
 plot(tgrid, dets$ytilde_k / sqrt(dets$S), xlab = "Time", ylab = "Standardized residual")
 
-create_forecast_df <- function(means,
-                               vars,
-                               target_type = "cases",
-                               fdt = forecast_date,
-                               location) {
-  # takes in a vector of means and variance for the normal predicted
-  # distribution of the next n weeks, and outputs a representation of
-  # this forecast in the standard covidhub format
-  if (target_type == "cases") {
-    probs <- c(0.025, 0.100, 0.250, 0.500, 0.750, 0.900, 0.975)
-    maxn <- 8
-    targ_string <- " wk ahead inc case"
-  } else{
-    stop("not implemented")
-    probs <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
-    if (target_type == "hosps") {
-      maxn <- 131
-    } else{
-      maxn <- 20
-    }
-  }
-  n <- nrow(means)
-  stopifnot(nrow(vars) == n)
-  stopifnot(n <= maxn)
-  wk_ahead <- seq_len(n)
-  targets <- paste0(wk_ahead, targ_string)
-  dte <- lubridate::ymd(fdt)
-  fdt_wd <- lubridate::wday(dte) # 1 = Sunday, 7 = Sat.
-  fdt_sun <- lubridate::ymd(dte) - (fdt_wd - 1)
-  take_back_step <- fdt_wd <= 2
-  if (take_back_step) {
-    week0_sun <- fdt_sun - 7
-  } else {
-    week0_sun <- fdt_sun
-  }
-  t1 <- expand_grid(quantile = probs, h = wk_ahead) %>%
-    mutate(target = paste0(h, targ_string),
-           target_end_date = week0_sun + 6 + h * 7) %>%
-    add_column(
-      location = as.character(location),
-      forecast_date = fdt,
-      type = "quantile"
-    ) %>%
-    mutate(q1 = map2_dbl(
-      quantile,
-      h,
-      ~ KScorrect::qmixnorm(
-        p = .x,
-        mean = means[.y,],
-        sd = sqrt(vars[.y,]),
-        pro = rep(1, ncol(means)),
-        expand = 0.0
-      )
-    ),
-    value = ifelse(q1 < 0, 0, q1)) %>%
-    mutate(value = format(value, digits = 4),
-           quantile = as.character(quantile))
-  t2 <- t1 %>% filter(quantile == "0.5") %>%
-    mutate(type = "point",
-           quantile = NA_character_)
-  bind_rows(t1, t2) %>% select(forecast_date,
-                               target,
-                               target_end_date,
-                               location,
-                               type,
-                               quantile,
-                               value)
-}
+
 
 inds <- which(fet$target_wday == 7)
 
@@ -426,3 +432,8 @@ fcst <- create_forecast_df(means = dets$sim_means[inds,],
 
 
 fcst %>% ggplot(aes(x = target_end_date, y = as.numeric(value), color = quantile)) + geom_line() + geom_point()
+
+fcst_path <- file.path("forecasts", paste0(forecast_date, "-", forecast_loc, "-CEID-InfectionKalman.csv"))
+if(!dir.exists("forecasts")) dir.create("forecasts")
+write_csv(x = fcst, path = fcst_path)
+
