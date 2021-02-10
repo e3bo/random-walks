a17361879107b0392654dd8cacb61b4aae960481
Eamon O'Dea <odea35@gmail.com>
2021-02-10 12:24:01 -0500

    Add plot of wis by forecast date for each lambda

--- analyze-scores.R
+++ analyze-scores.R
@@ -22,6 +22,7 @@ truth_data <- load_truth(truth_source = "JHU",
 scores <- score_forecasts(fdat2, truth_data, return_format = "long")
 ws <- scores %>% filter(score_name == "wis")
 
+
 ws1 <-
   ws %>% group_by(horizon, location, model, target_end_date) %>% 
   summarize(meanscore = mean(score_value), .groups = "drop")
@@ -38,16 +39,35 @@ ws2
 ws2 %>% ggplot(aes(x = horizon, y = meanscore, fill = model)) + 
   geom_col(position = position_dodge()) + 
   facet_grid(location ~ ., scales = "free_y") + 
-  labs(y = "wis")
+  labs(y = "WIS")
 
 ws3 <- ws %>% group_by(location, model) %>%
   summarize(meanscore = mean(score_value), .groups = "drop")
 ws3
 ws3 %>% ggplot(aes(x = location, y = meanscore, color = model)) + 
-  geom_point() + coord_flip() + labs(y = "wis")
+  geom_point() + coord_flip() + labs(y = "WIS")
 
 ws4 <- ws %>% group_by(model) %>% 
   summarize(meanscore = mean(score_value), .groups = "drop")
 ws4
 ws4 %>% ggplot(aes(x = model, y = meanscore)) + geom_col() + labs(y = "wis") +
   scale_x_discrete(guide = guide_axis(angle = 45))
+
+ws$lambda <- ws$model %>%
+  str_extract("lambda\\d{3}\\.\\d{2}") %>% str_remove("lambda") %>% as.numeric()
+
+ws5 <- ws %>% filter(str_detect(model, "InfectionKalman")) %>%
+  group_by(model, lambda, forecast_date, location) %>%
+  summarize(meanscore = mean(score_value), .groups = "drop") %>%
+  left_join(hub_locations, by = c("location" = "fips"))
+
+ws5 %>%
+  ggplot(aes(
+    x = forecast_date,
+    color = as.factor(lambda),
+    y = meanscore,
+    group = model
+  )) +
+  scale_y_continuous(trans = "log1p") + 
+  geom_point() + geom_line() + facet_wrap( ~ location_name, scales = "free_y") + 
+  labs(x = "forecast date", y = "WIS", color = expression(lambda))
