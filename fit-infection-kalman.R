a011caba09d33e51f1611f1936d618e8121c9993
Eamon O'Dea <odea35@gmail.com>
2021-04-09 23:13:21 -0400

    Decent fit to mobility and process noise covariates

--- fit-infection-kalman.R
+++ fit-infection-kalman.R
@@ -123,8 +123,8 @@ wfixed <- c(
 y <- wind %>% select(cases, hospitalizations, deaths)
 x <- wind %>% select(time, doses, prophome)
 x$dosesiqr <- x$doses /  diff(quantile(x$doses, c(.25, .75)))
-x$prophomeiqr <- x$prophome / diff(quantile(x$prophome, c(.25, .75)))
-#x$doses <- rnorm(x$doses)
+x$prophomeiqr <- (x$prophome - mean(x$prophome)) / diff(quantile(x$prophome, c(.25, .75)))
+x$dosesiqr <- rnorm(x$doses) # colinear with prophome in this case, so let's ignore
 
 
 
@@ -136,7 +136,7 @@ fit <- lbfgs::lbfgs(
   calc_kf_nll,
   calc_kf_grad,
   x = x,
-  betasd = 0.1,
+  betasd = 0.01,
   epsilon = 1e-1,
   max_iterations = 100,
   a = .9,
@@ -146,15 +146,15 @@ fit <- lbfgs::lbfgs(
   invisible = 0
 )
 
-system.time(h <- calc_kf_hess(w=fit$par, x=x, y=y, betasd=0.1, a =.1, pm = param_map))
+system.time(h <- calc_kf_hess(w=fit$par, x=x, y=y, betasd=0.01, a =.1, pm = param_map))
 rbind(winit, fit$par, diag(solve(h)))
 
 fit2 <- lbfgs::lbfgs(
   calc_kf_nll,
   calc_kf_grad,
   x = x,
-  betasd = 0.1,
-  epsilon = 1e-1,
+  betasd = 0.01,
+  epsilon = 1e-2,
   max_iterations = 100,
   a = .9,
   y = y,
@@ -164,6 +164,58 @@ fit2 <- lbfgs::lbfgs(
 )
 
 rbind(winit, fit$par, fit2$par)
+
+fit3 <- lbfgs::lbfgs(
+  calc_kf_nll,
+  calc_kf_grad,
+  x = x,
+  betasd = 0.01,
+  epsilon = 1e-3,
+  max_iterations = 100,
+  a = .9,
+  y = y,
+  pm = param_map,
+  fit2$par,
+  invisible = 0
+)
+
+system.time(h3 <- calc_kf_hess(w=fit3$par, x=x, y=y, betasd=0.01, a =.1, pm = param_map))
+rbind(winit, fit$par, fit2$par, fit3$par, diag(solve(h3)))
+
+fit4 <- lbfgs::lbfgs(
+  calc_kf_nll,
+  calc_kf_grad,
+  x = x,
+  betasd = 0.01,
+  epsilon = 1e-4,
+  max_iterations = 100,
+  a = .9,
+  y = y,
+  pm = param_map,
+  fit3$par,
+  invisible = 0
+)
+
+system.time(h4 <- calc_kf_hess(w=fit4$par, x=x, y=y, betasd=0.01, a =.1, pm = param_map))
+rbind(winit, fit$par, fit2$par, fit3$par, fit4$par, diag(solve(h4)))
+
+fit5 <- lbfgs::lbfgs(
+  calc_kf_nll,
+  calc_kf_grad,
+  x = x,
+  betasd = 0.001,
+  epsilon = 1e-5,
+  max_iterations = 400,
+  a = .9,
+  y = y,
+  pm = param_map,
+  fit4$par,
+  invisible = 0
+)
+
+system.time(h5 <- calc_kf_hess(w=fit5$par, x=x, y=y, betasd=0.001, a =.1, pm = param_map))
+rbind(winit, fit$par, fit2$par, fit3$par, fit4$par, fit5$par, diag(solve(h5)))
+
 
 fit_over_betagrid <- function(a, betasdgrid) {
   fits <- list()
@@ -242,7 +294,7 @@ save(x, y, wfixed, fits, fet, agrid, betasdgrid, file = the_file)
 ## 
 
 
-dets <- kf_nll_details(w=fit$par, x=x, y=y, betasd = .1, a = 0.9, pm = param_map, fet = NULL)
+dets <- kf_nll_details(w=fit5$par, x=x, y=y, betasd = .001, a = 0.9, pm = param_map, fet = NULL)
 
 par(mfrow = c(3, 1))
 qqnorm(dets$ytilde_k[1, ] / sqrt(dets$S[1, 1, ]), sub = "Cases")
@@ -278,7 +330,7 @@ lines(x$time, se_deaths * 2 + pred_deaths, col = "grey")
 lines(x$time, pred_deaths)
 lines(x$time,-se_deaths * 2 + pred_deaths, col = "grey")
 
-plot(exp(fit$par[-c(1:9)] + fit$par[8] * x$dosesiqr + fit$par[9] * x$prophomeiqr) / wfixed["gamma"], type = 'l', ylim = c(0, 2))
-lines(exp(fit$par[-c(1:9)] + fit$par[8] * x$dosesiqr + 0 * x$prophomeiqr) / wfixed["gamma"], col = 2)
-lines(exp(fit$par[-c(1:9)] + 0 * x$dosesiqr + fit$par[9] * x$prophomeiqr) / wfixed["gamma"], col = 3)
+plot(exp(fit5$par[-c(1:9)] + fit5$par[8] * x$dosesiqr + fit5$par[9] * x$prophomeiqr) / wfixed["gamma"], type = 'l')
+lines(exp(fit5$par[-c(1:9)] + fit5$par[8] * x$dosesiqr + 0 * x$prophomeiqr) / wfixed["gamma"], col = 2)
+lines(exp(fit$par[-c(1:9)] + 0 * x$dosesiqr + fit5$par[9] * x$prophomeiqr) / wfixed["gamma"], col = 3)
 
