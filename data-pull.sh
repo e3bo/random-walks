bf576b398f235bf385fdc2c853ee91dcd0d88f3a
Eamon O'Dea <odea35@gmail.com>
2021-04-22 17:09:00 -0400

    Stop data-pull.sh from requesting unavailable issues

--- data-pull.sh
+++ data-pull.sh
@@ -18,12 +18,9 @@ dvc run \
     --force \
     fdt=$fdt ./pull-hopkins-ts-from-date.R
 
-dvc run \
-    -d pull-hopkins-vaccine-ts-from-date.R \
-    -o hopkins-vaccine/$fdt \
-    -n hopkins-vaccine-$fdt \
-    --force \
-    fdt=$fdt ./pull-hopkins-vaccine-ts-from-date.R
+if [[ ${fdt} < "2020-12-01" ]]; then
+    exit 0
+fi
 
 locs=("01" "02" "04" "05" "06" "08" "09" "10" "11" "12" "13" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "44" "45" "46" "47" "48" "49" "50" "51" "53" "54" "55" "56")
 for loc in "${locs[@]}"
@@ -35,3 +32,14 @@ dvc run \
     --force \
     fdt=$fdt loc=$loc ./pull-healthdata-ts-from-date.R
 done
+
+if [[ ${fdt} < "2021-01-01" ]]; then
+    exit 0
+fi
+
+dvc run \
+    -d pull-hopkins-vaccine-ts-from-date.R \
+    -o hopkins-vaccine/$fdt \
+    -n hopkins-vaccine-$fdt \
+    --force \
+    fdt=$fdt ./pull-hopkins-vaccine-ts-from-date.R
