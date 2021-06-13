563a23aec58835a8af4ed9fd0041dd0745236ca3
Eamon O'Dea <odea35@gmail.com>
2021-06-13 19:37:21 -0400

    Tweak mase plot, add fitting time plot

--- plot-metrics.R
+++ plot-metrics.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-system("dvc metrics show > metrics.csv")
+library(tidyverse)
 
+system("dvc metrics show > metrics.csv")
 
 mets <- read.table("metrics.csv", header = TRUE, na.strings = "-") %>%
   mutate(date = str_extract(Path, "\\d{4}-\\d{2}-\\d{2}"),
@@ -11,7 +12,9 @@ mets <- read.table("metrics.csv", header = TRUE, na.strings = "-") %>%
 
 m2 <- mets %>% 
   filter(wday == "Mon" & loc2 == "06") %>% 
-  filter(fit1hessposdef == "True") %>%
+  filter(fit1hessposdef == "True") 
+
+m3 <- m2 %>%
   select(date, fit1mase_cases, fit1mase_cases_w, 
          fit1mase_hosps, fit1mase_hosps_w,
          fit1mase_death, fit1mase_death_w) %>%
@@ -23,9 +26,21 @@ m2 <- mets %>%
                           var2 == "death" ~ "Deaths", 
                           var2 == "hosps" ~ "Hospital admissions"))
 
-p <- ggplot(filter(m2, !is.na(value)), 
+p <- ggplot(filter(m3, !is.na(value)), 
        aes(x = lubridate::ymd(date), y = value, shape = Weekly)) + 
-  geom_point() + ylim(c(0, 1)) + facet_wrap(~var3) + 
-  labs(x = "Forecast date", y = "MASE")
+  geom_point() + ylim(c(0, 1)) + facet_wrap(~var3, ncol = 1) + 
+  labs(x = "Forecast date", y = "MASE") +
+  theme(legend.position="top")
 
-ggsave("california-mase.png", p, width = 7, units = "in")
+ggsave("california-mase.png", p, width = 3, units = "in")
+
+m4 <- m2 %>% select(date, fit1walltime) %>% 
+  mutate(date = lubridate::ymd(date),
+         hours = fit1walltime / 60 / 60)
+
+p2 <- m4 %>% ggplot(aes(x = date, y = hours)) + 
+  geom_point() +  
+  labs(x = "Forecast date", y = "Fitting time (hours)") +
+  theme_minimal()
+
+ggsave("california-fitting-time.png", p2, width = 3, height = 4, units = "in")
