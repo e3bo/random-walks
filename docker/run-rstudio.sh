8e9335501db75768d4c4e8b445956ef94e910cf7
Eamon O'Dea <odea35@gmail.com>
2021-06-24 16:19:02 -0400

    Correct user id for rocker container

--- docker/run-rstudio.sh
+++ docker/run-rstudio.sh
@@ -4,7 +4,7 @@ docker run \
 	-d \
 	--rm \
 	-p 8788:8787 \
-	-e USERID=1005 \
+	-e USERID=1001 \
 	-e PASSWORD=foo \
 	--mount type=bind,src=$HOME/src/random-walks,dst=/home/rstudio/work \
         --mount type=bind,src=$HOME/src/covid19-forecast-hub,dst=/home/rstudio/hub \
