edd470ae9e4b14c0dda3a0460a79aa6a5d2eb70c
Eamon O'Dea <odea35@gmail.com>
2021-07-01 16:54:09 -0400

    Update pipelines

--- step-forecasts-forward.sh
+++ step-forecasts-forward.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-fdtstart=2020-06-29; 
+fdtstart=2020-11-30; 
 for fdt in $(cat remaining-fdts); 
   do fdt=$fdt ./data-pull.sh && loc=06 fdt=$fdt fdtstart=$fdtstart ./forecast.sh; 
   fdtstart=$fdt; 
